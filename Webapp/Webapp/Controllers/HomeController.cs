a9abf31666646e5e989940c0228002bd47f3ae35
Marr95 <marwan1995@>
2022-06-13 14:35:03 -0700

    Project setup

---
+++ Webapp/Webapp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Webapp.dal;
+using Webapp.Models;
+
+namespace Webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BodyDataDAL bodyDataDAL;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            bodyDataDAL = new BodyDataDAL();
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            Task<string> bodyDatas = bodyDataDAL.getBodyDatasAsync();
+            string a = bodyDatas.Result;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Webapp/Webapp/dal/BodyDataDAL.cs
@@ -0,0 +1,19 @@
+﻿namespace Webapp.dal
+{
+    public class BodyDataDAL
+    {
+        public async Task<String> getBodyDatasAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var uri = new Uri("https://192.168.44.127:7272/BodyData");
+
+                var response = await client.GetAsync(uri);
+
+                string textResult = await response.Content.ReadAsStringAsync();
+
+                return textResult;
+            }
+        }
+    }
+}
